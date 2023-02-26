e072390fd34a43c083727d0149e98f62431692f3
Forzo <forzocoding@gmail.com>
2023-02-26 13:11:35 +0100

    0.0.4
    Added to "utility": "isNum"
    Implemented "isNum" to all the functions in "complex"

--- cryptographyComplements/complex.py
+++ cryptographyComplements/complex.py
@@ -1,5 +1,9 @@
+from cryptographyComplements.utility import isNumber # required, to check inputs entered by the user
+
 def EulerTotientFunction(number):
     "Calculate, from a given number, the Euler Totient function."
+    if not isNumber(number):
+        return None
 
     result = number
     p = 2
@@ -14,7 +18,10 @@ def EulerTotientFunction(number):
     return result
 
 def DiscreteLogarithm(base, congruence, modulo):
-    "Calculate, from given base, congreunce and modulo, the discrete logarithm."
+    "Calculate, from given base, congreunce and modulo, the discrete logarithm. \nWarning: The function won't stop even if there isn't a number that can resolve the discrete logarithm."
+    if not isNumber(base, congruence, modulo):
+        return None
+    
     BASE, congruence, modulo = int(base), int(congruence), int(modulo)
     power = 0
 
--- cryptographyComplements/utility.py
+++ cryptographyComplements/utility.py
@@ -13,3 +13,23 @@ def isEven(number):
     if number.endswith(evens):
         return True
     return False
+
+def isNumber(*args):
+    "Check if the input entered, can be converted to a number, or contains other characters."
+    check = []
+    for arg in args:
+        try:
+            arg = int(arg)
+            checkNum = True
+
+        except ValueError as e:
+            print(f"Cryptography Complements: {e}")
+            checkNum = False
+        
+        check.append(checkNum)
+
+    if False in check:
+        print(f"Cryptography Complements: One or more input, are not numbers.")
+        return False
+
+    return True
