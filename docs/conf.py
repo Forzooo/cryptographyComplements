3a62b103f9f9833e19a682648bda9259e4745df8
Forzo <forzocoding@gmail.com>
2023-03-10 22:22:39 +0100

    docs

---
+++ docs/conf.py
@@ -0,0 +1,16 @@
+# Configuration file for the Sphinx documentation builder.
+
+project = 'cryptographyComplements'
+copyright = '2023, Forzo'
+author = 'Forzo'
+release = 'latest'
+
+extensions = []
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+html_theme = 'furo'
+html_static_path = ['_static']
+
+master_doc = 'index'
